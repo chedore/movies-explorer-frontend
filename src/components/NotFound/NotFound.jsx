42469a7ba257211e824fed492a69bf07c4fa3101
Svetlana Prozhirova <svetakesha@yandex.ru>
2023-09-08 16:31:25 +0300

    feat: страница 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -6,11 +6,12 @@ export default function NotFound() {
   return (
     <main className="main">
       <h1 className="main__title">404</h1>
-      <p className='main__description'>Страница не найдена</p>
+      <p className="main__description">Страница не найдена</p>
       <button
-        className='main__button'
-        onClick={() => navigate("/", {replace: true})}
-        type='button'>
+        className="button main__button"
+        onClick={() => navigate("/", { replace: true })}
+        type="button"
+      >
         Назад
       </button>
     </main>
