c980898017f86cf5a08c39b2467e6f56da5dd8ee
Svetlana Prozhirova <svetakesha@yandex.ru>
2023-09-19 09:08:12 +0300

    feat: обновление страницы profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import "./Profile.css";
 import React, { useState } from "react";
 import Header from "../Header/Header";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const [name, setName] = useState("Виталий");
@@ -9,6 +10,26 @@ export default function Profile() {
   return (
     <main className="profile">
       <Header />
+      <main>
+        <h1 className="profile__title">Привет, {name}!</h1>
+
+        <div className="profile__container">
+          <div className="profile__element">
+            <label className="profile__name">Имя</label>
+            <label className="profile__input">{name}</label>
+          </div>
+          <div className="profile__element">
+            <label className="profile__name">E-mail</label>
+            <label className="profile__input">{email}</label>
+          </div>
+        </div>
+        <div className="profile__buttons">
+          <button className="button profile__button-edit">Редактировать</button>
+          <Link className="link profile__link-exit" to="/signin">
+            Выйти из аккаунта
+          </Link>
+        </div>
+      </main>
     </main>
   );
 }
